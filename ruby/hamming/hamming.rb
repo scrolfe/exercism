28149eaeb43d0ffb87968a1882ce08afd8384034
Sam Rolfe <scrolfe@gmail.com>
2017-11-06 12:04:17 -0600

    first test

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,28 @@
-hamming = Class.new do
-	def compute(a, b)
-		p "a is ", a, "b is ", b 
+class Hamming
+
+	@match = 0
+
+	@arr_a = [] 
+	@arr_b = [] 
+
+
+	def self.compute(a, b)
+
+		@arr_a = a.split("")
+		@arr_b = b.split("")
+
+ 		@arr_a.each_index do |index|		
+			if @arr_a[index] == @arr_b[index]
+				@match += 1	
+				p 'hit'
+			end
+		end
+
+		p @match
+		return @match
+
 	end
+
 end
 
-results = hamming.new
-results.compute("abc", "def")
-
+results = Hamming.new
