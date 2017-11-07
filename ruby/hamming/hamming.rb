a9f3ccb09589b97dd5742c03cfb9d8cf923985d5
Sam Rolfe <scrolfe@gmail.com>
2017-11-07 13:57:37 -0600

    all assertions passing

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,6 +3,9 @@ class Hamming
 		match = 0
 		arr_a = a.split("")
 		arr_b = b.split("")
+		if arr_a.length != arr_b.length
+			raise ArgumentError
+		end
  		arr_a.each_index do |index|		
 			if arr_a[index] != arr_b[index]
 				match += 1	
--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -31,52 +31,52 @@ class HammingTest < Minitest::Test
   end
 
   def test_small_distance_in_small_strands
-    skip
+    # skip
     assert_equal 1, Hamming.compute('AT', 'CT')
   end
 
   def test_small_distance
-    skip
+    # skip
     assert_equal 1, Hamming.compute('GGACG', 'GGTCG')
   end
 
   def test_small_distance_in_long_strands
-    skip
+    # skip
     assert_equal 2, Hamming.compute('ACCAGGG', 'ACTATGG')
   end
 
   def test_non_unique_character_in_first_strand
-    skip
+    # skip
     assert_equal 1, Hamming.compute('AAG', 'AAA')
   end
 
   def test_non_unique_character_in_second_strand
-    skip
+    # skip
     assert_equal 1, Hamming.compute('AAA', 'AAG')
   end
 
   def test_same_nucleotides_in_different_positions
-    skip
+    # skip
     assert_equal 2, Hamming.compute('TAG', 'GAT')
   end
 
   def test_large_distance
-    skip
+    # skip
     assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
   end
 
   def test_large_distance_in_off_by_one_strand
-    skip
+    # skip
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 
   def test_disallow_first_strand_longer
-    skip
+    # skip
     assert_raises(ArgumentError) { Hamming.compute('AATG', 'AAA') }
   end
 
   def test_disallow_second_strand_longer
-    skip
+    # skip
     assert_raises(ArgumentError) { Hamming.compute('ATA', 'AGTG') }
   end
 
